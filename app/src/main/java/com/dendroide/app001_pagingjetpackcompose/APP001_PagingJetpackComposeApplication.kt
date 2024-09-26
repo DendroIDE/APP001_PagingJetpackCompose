358cb9282a14318ac976020c6c2a9e9c66015b2c
DendroIDE <dendroide.code@gmail.com>
2024-09-26 03:59:08 -0500

    hilt-navigation

---
+++ app/src/main/java/com/dendroide/app001_pagingjetpackcompose/APP001_PagingJetpackComposeApplication.kt
@@ -0,0 +1,7 @@
+package com.dendroide.app001_pagingjetpackcompose
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class APP001_PagingJetpackComposeApplication : Application ()
---
+++ app/src/main/java/com/dendroide/app001_pagingjetpackcompose/presentation/RickListScreen.kt
@@ -0,0 +1,8 @@
+package com.dendroide.app001_pagingjetpackcompose.presentation
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RickListScreen(){
+
+}
